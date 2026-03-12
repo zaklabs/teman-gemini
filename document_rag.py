6afdaa97b360485001a1f158b47a516a3b890d5d
Zaki Fuadi <41351878+zaklabs@users.noreply.github.com>
2026-03-12 02:55:51 +0000

    Refactor imports in document_rag.py and update requirements.txt for langchain-classic

--- document_rag.py
+++ document_rag.py
@@ -24,10 +24,10 @@ Untuk membaca dan memproses dokumen PDF menggunakan Google Gemini
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.schema import Document
+from langchain_core.documents import Document
 import tempfile
 import os
 import shutil
